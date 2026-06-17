6a8ddf64a5faf4a15d98023f2367d39421c30cdf
HRNPH <hrnph@protonmail.com>
2026-06-17 22:53:27 +0700

    chore: update fork config comment, remove unused constant

--- packages/server/src/constants/fork-config.ts
+++ packages/server/src/constants/fork-config.ts
@@ -1,7 +1,8 @@
 /**
  * Fork configuration for Dokploy-Ench
  * Change these values to match your fork's Docker image and GitHub repo.
+ *
+ * Version scheme: v10.x.x (avoids collision with upstream v0.x.x)
  */
 export const FORK_DOCKER_IMAGE = "ghcr.io/hrnph/dokploy-ench";
 export const FORK_GITHUB_REPO = "HRNPH/dokploy-ench";
-export const FORK_DOCKERHUB_API = `https://hub.docker.com/v2/repositories/${FORK_DOCKER_IMAGE.replace("ghcr.io/", "")}/tags`;
